cb8fa911e152872e25f594f7a458c2c5d83c385a
carlos <carlosjmnz79@gmail.com>
2017-11-09 10:08:54 -0300

    After changing views ... not working.

--- ewatch/early/urls.py
+++ ewatch/early/urls.py
@@ -1,7 +1,45 @@
+# Initial view
+
+# from django.conf.urls import url
+
+# from . import views
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+# ]
+
+#-----------------------------------------------------
+# Work in progress
+
 from django.conf.urls import url
 
 from . import views
 
+app_name = 'early'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # ex: /early/
+    url(r'^$', views.CountriesView.as_view(), name='countries'),
+    # ex: /early/CHILE/
+    url(r'^(?P<pk>[A-Z]+)/$', views.ViewView.as_view(), name='view'),
+    # ex: /early/CHILE/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 ]
+#-----------------------------------------------------
+
+#	From app report
+
+# from django.conf.urls import url
+
+# from . import views
+
+# app_name = 'report'
+# urlpatterns = [
+#     # ex: /report/
+#     url(r'^$', views.IndexView.as_view(), name='index'),
+#     # ex: /report/5/
+#     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+#     # ex: /report/5/results/
+#     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+#     # ex: /report/5/vote/
+#     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+# ]
--- ewatch/early/views.py
+++ ewatch/early/views.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+# Initial
 
-# from django.shortcuts import render
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -13,3 +15,28 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the early index.")
+
+
+# ----------------------------------------------------------------
+# # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
+
+# # from django.shortcuts import render
+
+# # # Create your views here.
+
+# from django.shortcuts import get_object_or_404, render
+# from django.http import HttpResponseRedirect
+# from django.urls import reverse
+# from django.views import generic
+
+# from .models import Country, View
+
+# class IndexView(generic.ListView):
+#     model = Country
+#     template_name = 'early/index.html'
+#     # Sin embargo, para ListView, la variable contextual generada de forma automática es country_list
+
+#     def get_queryset(self):
+#         """ """
+#         return Country.objects.order_by('country_text')
