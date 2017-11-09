c3ce03a976c24df9b84ee0992e1ab90f47f33c83
carlos <carlosjmnz79@gmail.com>
2017-11-09 11:19:00 -0300

    Previous to detail view function creation.

--- ewatch/early/urls.py
+++ ewatch/early/urls.py
@@ -1,7 +1,27 @@
+# FROM REPORT
+
 from django.conf.urls import url
 
 from . import views
 
+app_name = 'early'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # ex: /early/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /early/5/
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # ex: /early/5/results/
+    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # ex: /early/5/vote/
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
+
+# ORIGINAL
+
+# from django.conf.urls import url
+
+# from . import views
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+# ]
--- ewatch/early/views.py
+++ ewatch/early/views.py
@@ -3,13 +3,33 @@
 
 # from django.shortcuts import render
 
+# FROM REPORT
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Country, View
+
+
+class IndexView(generic.ListView):
+    template_name = 'early/index.html'
+    # context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Country.objects.order_by('country_text')
+
+# ORIGINAL
+
 # Create your views here.
 
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the early index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the early index.")
